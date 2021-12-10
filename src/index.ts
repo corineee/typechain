7f5f89726d1af73fcf625081ecda275ce259de22
b970311 <b970311@yonsei.ac.kr>
2021-12-10 15:43:22 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `Hello ${name}, you ar ${age}, you are a ${gender}`;
+};
+
+console.log(sayHi("Nicolas", 24, "male "));
+
+export {};
